f9ffe1a1848fdf8626863721b376747b3533f277
ferwin <ferwin.arias@gmail.com>
2021-12-01 17:53:11 -0500

    settings to deploy

---
+++ client/build/precache-manifest.da0f009782957892b20c5415d9fef7c6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6335e296ffa21d2c845a873d1bd5ddcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0804660051454e3ba5ed",
+    "url": "/static/css/main.cb9d9fbc.chunk.css"
+  },
+  {
+    "revision": "0fb5d3d2c97323925feb",
+    "url": "/static/js/2.2e52e54c.chunk.js"
+  },
+  {
+    "revision": "e364af75fff56a5298ee87761858e88e",
+    "url": "/static/js/2.2e52e54c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63cf7dce98fa50ce8bf3",
+    "url": "/static/js/3.e04f9a92.chunk.js"
+  },
+  {
+    "revision": "0804660051454e3ba5ed",
+    "url": "/static/js/main.09fd4c4e.chunk.js"
+  },
+  {
+    "revision": "d8a4bffe9bf21ac13450",
+    "url": "/static/js/runtime-main.bd19b990.js"
+  },
+  {
+    "revision": "4306e14f605a1364bf738181dce0780f",
+    "url": "/static/media/dog.4306e14f.png"
+  },
+  {
+    "revision": "8bdc8af3eb2da1d5a68b6288bf4c779c",
+    "url": "/static/media/pattern3.8bdc8af3.png"
+  },
+  {
+    "revision": "42511b297771659c1377498305597ecd",
+    "url": "/static/media/perrito_default.42511b29.jpeg"
+  },
+  {
+    "revision": "5a3fc00b1145999527f1f898eba39300",
+    "url": "/static/media/pexels-anna-bondarenko-6588292.5a3fc00b.mp4"
+  },
+  {
+    "revision": "4b9d794078a8d9d3ef3ea620b6705a09",
+    "url": "/static/media/pngtree-cute-corgi-crying-dog-cartoon-image_2299953.4b9d7940.jpg"
+  }
+]);
