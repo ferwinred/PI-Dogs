4230e8f2eb6b8a6fd85c2852dd4763032af132ed
ferwin <ferwin.arias@gmail.com>
2021-12-02 17:35:58 -0500

    adding settings

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const helmet = require('helmet');
-const cors = require('cors');
 
 require('./db.js');
+
 
 const server = express();
 
@@ -24,36 +22,8 @@ server.use((req, res, next) => {
   next();
 });
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://shrouded-journey-38552.herokuapp.com']
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    };
-  }
-};
-
-server.use(helmet());
-// --> Add this
-server.use(cors(corsOptions));
-
 
 server.use('/api', routes);
-
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  server.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  server.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-};
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
